388f345f0280f213cd9f6fe5ee992019b18cb8ab
Peter Bayne <peter.bayne@haumohio.com>
2022-01-25 15:19:29 +1300

    Ch 14 Dealing solitaire

--- _includes/src/ch14.fs
+++ _includes/src/ch14.fs
@@ -1,4 +1,29 @@
 module Solitaire
 open System
+open Core
 
-let x = 1
+type Game = {
+  deck: Card list
+  table: Card list
+  stacks: Card list list
+}
+
+let deal shuffledDeck = 
+  let emptyGame = {
+    deck = shuffledDeck
+    table = []
+    stacks = []
+  }
+              // a list of numbers from 6 to 1 (inclusive)
+  [6..-1..1]  // stepping at -1 intervals (i.e. counting down)
+  |>  List.fold (fun game i -> 
+        {
+          stacks = game.stacks @ [ game.deck |> List.take i ]
+          deck = game.deck |> List.skip i
+          table = []
+        }
+      
+      ) emptyGame
+
+//  Let do a deal!
+newDeck |> shuffle |> deal
